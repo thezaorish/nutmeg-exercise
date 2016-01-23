a57fb2fb4c3a4bdde14860dad62719edf28fab4d
Victor Balanica <victorbalanica@gmail.com>
2016-01-23 19:22:51 +0000

    basic setup of the maven project

---
+++ src/main/java/com/thezaorish/nutmeg/Runner.java
@@ -0,0 +1,12 @@
+package com.thezaorish.nutmeg;
+
+/**
+ * Created by zaorish on 23/01/16.
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		System.out.print("test");
+	}
+
+}
